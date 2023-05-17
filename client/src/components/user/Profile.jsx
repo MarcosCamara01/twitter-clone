95db350de2317378dc6651a848f3ae757a13b0bd
MarcosCamara01 <marcospenelascamara@gmail.com>
2023-05-17 21:26:22 +0200

    twitter-clone update

--- client/src/components/user/Profile.jsx
+++ client/src/components/user/Profile.jsx
@@ -1,11 +1,10 @@
 import React, { useEffect, useState } from 'react';
-import avatar from "../../assets/img/user.png";
-import { NavLink, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { Global } from '../../helpers/Global';
-import { Link } from 'react-router-dom';
 import { useAuth } from '../../hooks/useAuth';
 import { PublicationList } from '../publication/PublicationList';
 import { GetProfile } from '../../helpers/GetProfile';
+import Header from '../../helpers/Header';
 
 export const Profile = () => {
 
@@ -36,9 +35,9 @@ export const Profile = () => {
         setLoading(true);
         let dataUser = await GetProfile(params.userId, setUser);
         if (dataUser.following && dataUser.following._id) {
-            setIFollow(true); 
+            setIFollow(true);
             setLoading(false);
-        } 
+        }
     }
 
     const getCounters = async () => {
@@ -60,40 +59,7 @@ export const Profile = () => {
         setLoading(false);
     }
 
-    const follow = async (userId) => {
-        const request = await fetch(Global.url + "follow/save", {
-            method: "POST",
-            body: JSON.stringify({ followed: userId }),
-            headers: {
-                "Content-Type": "application/json",
-                "Authorization": localStorage.getItem("token")
-            }
-        });
-
-        const data = await request.json();
-
-        if (data.status == "success") {
-            setIFollow(true);
-        }
-    }
-
-    const unfollow = async (userId) => {
-        const request = await fetch(Global.url + "follow/unfollow/" + userId, {
-            method: "DELETE",
-            headers: {
-                "Content-Type": "application/json",
-                "Authorization": localStorage.getItem("token")
-            }
-        });
-
-        const data = await request.json();
-
-        if (data.status == "success") {
-            setIFollow(false);
-        }
-    }
-
-    const getPublications = async (page = 1, newProfile = false) => {
+    const getPublications = async (page = 1, newProfile = true) => {
         setLoading(true);
         const request = await fetch(Global.url + "publication/user/" + params.userId + "/" + page, {
             method: "GET",
@@ -132,70 +98,49 @@ export const Profile = () => {
         setLoading(false);
     }
 
+    const follow = async (userId) => {
+        const request = await fetch(Global.url + "follow/save", {
+            method: "POST",
+            body: JSON.stringify({ followed: userId }),
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": localStorage.getItem("token")
+            }
+        });
+
+        const data = await request.json();
+
+        if (data.status == "success") {
+            setIFollow(true);
+        }
+    }
+
+    const unfollow = async (userId) => {
+        const request = await fetch(Global.url + "follow/unfollow/" + userId, {
+            method: "DELETE",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": localStorage.getItem("token")
+            }
+        });
+
+        const data = await request.json();
+
+        if (data.status == "success") {
+            setIFollow(false);
+        }
+    }
+
     return (
         <>
-            <header className="aside__profile-info">
-
-                <div className="profile-info__general-info">
-                    <div className="general-info__container-avatar">
-                        {
-                            user.image === "default.png" 
-                            ?
-                            <img src={avatar} className='profile__container-avatar' alt='profile picture' />
-                            :
-                            <img src={Global.url + "user/avatar/" + user.image} className="profile__container-avatar" alt="profile picture" />
-
-                        }
-                    </div>
-
-                    <div className="general-info__container-names">
-                        <div className="container-names__name">
-                            <h1>{user.name} {user.surname}</h1>
-                            {user._id != auth._id ?
-                                (iFollow ?
-                                    <button onClick={() => unfollow(user._id)} className="post__button"><div></div></button>
-                                    :
-                                    <button onClick={() => follow(user._id)} className="post__button--white">Follow</button>
-                                )
-                                :
-                                <>
-                                    <button className="post__button post__button--normal">
-                                        <NavLink to="/social/settings">Edit profile</NavLink>
-                                    </button>
-                                </>
-                            }
-                        </div>
-                        <h2 className="container-names__nickname">{" @" + user.nick}</h2>
-                        <p>{user.bio}</p>
-                    </div>
-                </div>
-
-                <div className="profile-info__stats">
-
-                    <div className="stats__following">
-                        <Link to={"/social/following/" + user._id} className="following__link">
-                            <span className="following__title">Following</span>
-                            <span className="following__number">{counters.following >= 1 ? counters.following : 0}</span>
-                        </Link>
-                    </div>
-                    <div className="stats__following">
-                        <Link to={"/social/followers/" + user._id} className="following__link">
-                            <span className="following__title">Followers</span>
-                            <span className="following__number">{counters.followed >= 1 ? counters.followed : 0}</span>
-                        </Link>
-                    </div>
-
-
-                    <div className="stats__following">
-                        <Link to={"/social/profile/" + user._id} className="following__link">
-                            <span className="following__title">Publications</span>
-                            <span className="following__number">{counters.publications >= 1 ? counters.publications : 0}</span>
-                        </Link>
-                    </div>
-
-
-                </div>
-            </header>
+            <Header
+                user={user}
+                auth={auth}
+                iFollow={iFollow}
+                unfollow={unfollow}
+                follow={follow}
+                counters={counters}
+            />
 
             <PublicationList
                 publications={publications}
