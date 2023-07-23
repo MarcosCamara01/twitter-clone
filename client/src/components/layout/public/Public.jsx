5bb7544f32f2fa340e69a2f56cee04e2cbb830fc
MarcosCamara01 <marcospenelascamara@gmail.com>
2023-07-23 21:51:14 +0200

    Public section loading effect

--- client/src/components/layout/public/Public.jsx
+++ client/src/components/layout/public/Public.jsx
@@ -2,20 +2,25 @@ import React from 'react'
 import { Header } from './Header'
 import { Navigate, Outlet } from 'react-router-dom'
 import { useAuth } from '../../../hooks/useAuth'
+import { BsTwitter } from 'react-icons/bs';
 
 export const Public = () => {
 
-  const { auth } = useAuth();
+  const { auth, loading } = useAuth();
 
-  return (
-    <>
-      <Header />
+  if (loading) {
+    return <div className='twitter-logo-bx'><BsTwitter /></div>
+  } else {
+    return (
+      <>
+        <Header />
 
-      <section className='layout__content'>
-        {
-          !auth._id ? <Outlet /> : <Navigate to="/social" />
-        }
-      </section>
-    </>
-  )
+        <section className='layout__content'>
+          {
+            !auth._id ? <Outlet /> : <Navigate to="/social" />
+          }
+        </section>
+      </>
+    )
+  }
 }
--- client/src/components/publication/PublicationList.jsx
+++ client/src/components/publication/PublicationList.jsx
@@ -28,7 +28,6 @@ export const PublicationList = ({ publications, getPublications, page, setPage, 
         });
 
         const data = await request.json();
-        console.log(data)
         if (data.status === "success") {
             getPublications(1, true);
             setPage(1);
