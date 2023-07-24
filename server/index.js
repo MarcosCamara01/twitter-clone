c89ab12805490fe2cefae48a7083ed55c30b8942
MarcosCamara01 <marcospenelascamara@gmail.com>
2023-07-24 19:32:51 +0200

    cors

--- server/index.js
+++ server/index.js
@@ -1,26 +1,20 @@
-// Importar dependencias
 const connection = require("./database/connection");
 const express = require("express");
 const cors = require("cors");
 require('dotenv').config();
 
-// Conexion a bbdd
 connection();
 
-// Crear servidor node
 const app = express();
 const puerto = process.env.PORT || 3900;
 
-// Configurar cors
 app.use(cors({
-  origin: 'http://localhost:5173'
+  origin: 'https://twitter-clone-aca36.web.app'
 }));
 
-// Convertir los datos del body a objetos js
 app.use(express.json());
 app.use(express.urlencoded({extended: true})); 
 
-// Cargar conf rutas
 const UserRoutes = require("./routes/user");
 const PublicationRoutes = require("./routes/publication");
 const FollowRoutes = require("./routes/follow");
@@ -29,7 +23,6 @@ app.use("/api/user", cors(), UserRoutes);
 app.use("/api/publication", cors(), PublicationRoutes);
 app.use("/api/follow", cors(), FollowRoutes);
 
-// Poner servidor a escuchar peticiones http
 app.listen(puerto, "0.0.0.0", () => {
     console.log("Servidor de node corriendo en el puerto: ", puerto);
 });
